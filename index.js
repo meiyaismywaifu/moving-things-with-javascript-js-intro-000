a744116e00de440c0e263c8d514f57d4803f5952
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-21 23:45:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const app = "I don't do much."
 //65 is A, 68 is D.
 
 var dodger = document.getElementById('dodger');
+var width = $("dodger").css("width");
+  // for some reason "dodger.style.width" doesn't access it.
 
 document.addEventListener('keydown', (e)=>{
 
   var leftEdge = parseInt(dodger.style.left.replace('px',''), 10)
-  var rightEdge = parseInt(dodger.style.right.replace('px',''), 10)
+  var rightEdge = leftEdge + width;
 
   if (e.which === 65 && leftEdge > 0){
     dodger.style.left= `${leftEdge - 5}px`;
