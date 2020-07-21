4e839f3f53a3c0a2e0a672368010978c235f9f9d
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-21 23:55:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const app = "I don't do much."
 //65 is A, 68 is D.
 
-var dodger = document.getElementById('dodger');
-var width = parseInt($("#dodger").css("width").replace('px',''), 10);
-  // for some reason "dodger.style.width" doesn't access it.
-  // "dodger.style.right" gives null value, doesn't autocalc.
-var leftEdge = parseInt(dodger.style.left.replace('px',''), 10)
-var rightEdge = leftEdge + width;
-var rightFrame = parseInt($("#game").css("width").replace('px',''), 10);
+
 
 document.addEventListener('keydown', (e)=>{
+
+  var dodger = document.getElementById('dodger');
+  var width = parseInt($("#dodger").css("width").replace('px',''), 10);
+    // for some reason "dodger.style.width" doesn't access it.
+    // "dodger.style.right" gives null value, doesn't autocalc.
+  var leftEdge = parseInt(dodger.style.left.replace('px',''), 10)
+  var rightEdge = leftEdge + width;
+  var rightFrame = parseInt($("#game").css("width").replace('px',''), 10);
+  
   if (e.which === 65 && leftEdge > 0){
     console.log("moving left");
     dodger.style.left= `${leftEdge - 5}px`;
