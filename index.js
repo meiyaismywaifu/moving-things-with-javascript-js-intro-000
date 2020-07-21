ca6e29193191eff979cb3ec790f5a45a6b022f87
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-21 23:19:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const app = "I don't do much."
 //65 is A, 68 is D.
+
 var dodger = document.getElementById('dodger');
+
 document.addEventListener('keydown', (e)=>{
-  console.log(e.which);
+  if (e.which === 65){
+    var leftNum = dodger.style.replace('px','')
+    var left = parseInt(leftNumbers, 10)
+    dodger.style.left= `${left - 1}px`;
+  } else if (e.which === 68){
+    
+  }
 })
