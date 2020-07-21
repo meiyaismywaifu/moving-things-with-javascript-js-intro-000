b222b2d568067c5e1ce53206ff97324659e094c2
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-21 23:20:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const app = "I don't do much."
 var dodger = document.getElementById('dodger');
 
 document.addEventListener('keydown', (e)=>{
+  var left = parseInt(leftNumbers, 10)
+  
   if (e.which === 65 && left > 0){
     var leftNum = dodger.style.replace('px','')
-    var left = parseInt(leftNumbers, 10)
     dodger.style.left= `${left - 1}px`;
   } else if (e.which === 68){
 
